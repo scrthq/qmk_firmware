df3adc293f4a19f842077f56508506573a990114
Nate Ferrell <nate@scrthq.com>
2019-10-16 04:06:22 -0500

    updated preonic keymap

--- keyboards/preonic/keymaps/scrthq/keymap.c
+++ keyboards/preonic/keymaps/scrthq/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   MEH_T(KC_EQL),  KC_1,             KC_2,    KC_3,    KC_4,             KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,         KC_Q,             KC_W,    KC_E,    KC_R,             KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_MINS,        LT(_ADJUST,KC_A), KC_S,    KC_D,    LT(_ADJUST,KC_F), KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSPO,        LT(_ADJUST,KC_Z), KC_X,    KC_C,    KC_V,             KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
+  KC_LSPO,        LCTL_T(KC_Z),     KC_X,    KC_C,    KC_V,             KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
   HYPR_T(KC_GRV), KC_LCTL,          KC_LGUI, KC_LALT, LOWER,            KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 

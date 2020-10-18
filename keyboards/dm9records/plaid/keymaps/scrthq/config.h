631601637d3552f828bb902c9764636ad6864c02
Nate Ferrell <nate@scrthq.com>
2020-10-18 16:38:31 -0500

    added secrets.h to .gitignore, keymap for plaid and userspace folder

--- keyboards/dm9records/plaid/keymaps/scrthq/config.h
+++ keyboards/dm9records/plaid/keymaps/scrthq/config.h
@@ -1,6 +1,10 @@
 // These VID/PID is not verified. Use at your own risk.
 #undef VENDOR_ID
 #undef PRODUCT_ID
+#undef DYNAMIC_KEYMAP_LAYER_COUNT
 
 #define VENDOR_ID 0x0d39
 #define PRODUCT_ID 0x0001
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define GRAVE_ESC_CTRL_OVERRIDE
--- keyboards/dm9records/plaid/keymaps/scrthq/keymap.c
+++ keyboards/dm9records/plaid/keymaps/scrthq/keymap.c
@@ -16,8 +16,24 @@
 
 #include QMK_KEYBOARD_H
 
-#define LOWER  FN_MO13
-#define RAISE  FN_MO23
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
+
+enum plaid_layers {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _FUNCTION,
+  _HOLDF,
+  _HOLDJ
+};
+
+enum plaid_keycodes {
+    MOFO = SAFE_RANGE,
+    ALT_TAB = SAFE_RANGE,
+    LOWER = 0x5F10,
+    RAISE
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -25,18 +41,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |     |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl | RAlt | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[0] = LAYOUT_plaid_grid(
+/* [0] = LAYOUT_plaid_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
     KC_LCTL, KC_RALT, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+), */
+[_QWERTY] = LAYOUT_plaid_grid(
+    KC_GESC, KC_Q,    KC_W,    KC_E,        KC_R,       KC_T,    KC_Y,    KC_U,       KC_I,       KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,  KC_A,    KC_S,    KC_D,        LT(4,KC_F), KC_G,    KC_H,    LT(5,KC_J), KC_K,       KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSPO, KC_Z,    KC_X,    KC_C,        KC_V,       KC_B,    KC_N,    KC_M,       KC_COMM,    KC_DOT,  KC_SLSH, KC_RSPC,
+    KC_LCTL, KC_LGUI, KC_RALT, HYPR(KC_NO), LOWER,      KC_SPC,  KC_SPC,  RAISE,      MEH(KC_NO), KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Lower
@@ -50,11 +72,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[1] = LAYOUT_plaid_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
+/* [1] = LAYOUT_plaid_grid(
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+), */
+[_LOWER] = LAYOUT_plaid_grid(
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
+    KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LT,   KC_GT,   KC_PGUP, KC_ENT,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ENT,  KC_ENT,  KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END
 ),
 
 /* Raise
@@ -68,14 +96,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[2] = LAYOUT_plaid_grid(
+/* [2] = LAYOUT_plaid_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+), */
+[_RAISE] = LAYOUT_plaid_grid(
+    KC_GRV,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,    KC_9,     KC_0,     KC_BSPC,
+    KC_TAB,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MINS,  KC_EQL,  KC_LBRC,  KC_RBRC,  KC_BSLS,
+    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_UP,    KC_ENT,
+    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_ENT,   KC_ENT,   KC_TRNS,  KC_TRNS, KC_LEFT,  KC_DOWN,  KC_RGHT
 ),
 
-/*
+/* Function
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -86,14 +120,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[3] = LAYOUT_plaid_grid(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+[_FUNCTION] = LAYOUT_plaid_grid(
+    RESET,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_PSCR,
+    KC_DEL,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  MOFO,     KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
+    TO(4),    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_ENT,   KC_ENT,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS
 ),
 
-/*
+/* Hold F
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -104,16 +138,136 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[4] = LAYOUT_plaid_grid(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+[_HOLDF] = LAYOUT_plaid_grid(
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+    KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LT,   KC_GT,   KC_PGUP, KC_ENT,
+    TO(0),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ENT,  KC_ENT,  KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END
+),
+
+/* Hold J
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_HOLDJ] = LAYOUT_plaid_grid(
+    KC_GRV,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,    KC_9,     KC_0,     KC_BSPC,
+    KC_TAB,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MINS,  KC_EQL,  KC_LBRC,  KC_RBRC,  KC_BSLS,
+    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_UP,    KC_ENT,
+    TO(0),   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_ENT,   KC_ENT,   KC_TRNS,  KC_TRNS, KC_LEFT,  KC_DOWN,  KC_RGHT
 )
 
 };
+
 bool led_update_user(led_t led_state) {
-    writePin(LED_RED, led_state.caps_lock);
-    writePin(LED_GREEN, led_state.scroll_lock);
+    /* writePin(LED_RED, led_state.caps_lock); */
     return false;
 }
+
+void keyboard_post_init_user(void) {
+    writePin(LED_GREEN, 1);
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case _QWERTY:
+        writePin(LED_GREEN, 1);
+        writePin(LED_RED, 0);
+        break;
+    case _LOWER:
+        writePin(LED_GREEN, 0);
+        writePin(LED_RED, 1);
+        break;
+    case _RAISE:
+        writePin(LED_GREEN, 1);
+        writePin(LED_RED, 1);
+        break;
+    case _FUNCTION:
+        writePin(LED_GREEN, 1);
+        writePin(LED_RED, 0);
+        break;
+    case _HOLDF:
+        writePin(LED_GREEN, 1);
+        writePin(LED_RED, 1);
+        break;
+    case _HOLDJ:
+        writePin(LED_GREEN, 0);
+        writePin(LED_RED, 0);
+        break;
+    default: //  for any other layers, or the default layer
+        writePin(LED_GREEN, 1);
+        writePin(LED_RED, 0);
+        break;
+    }
+    return state;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case MOFO:
+        if (record->event.pressed) {
+            // when keycode MOFO is pressed
+            SEND_STRING("Motherfucker from QMK!");
+        }
+        break;
+    case LOWER:
+        if (record->event.pressed) {
+        layer_on(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _FUNCTION);
+        } else {
+            layer_off(_LOWER);
+            update_tri_layer(_LOWER, _RAISE, _FUNCTION);
+        }
+        break;
+    case RAISE:
+        if (record->event.pressed) {
+            layer_on(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _FUNCTION);
+        } else {
+            layer_off(_RAISE);
+            update_tri_layer(_LOWER, _RAISE, _FUNCTION);
+        }
+        break;
+    case ALT_TAB:
+        if (record->event.pressed) {
+            if (!is_alt_tab_active) {
+            is_alt_tab_active = true;
+            register_code(KC_LALT);
+            }
+            alt_tab_timer = timer_read();
+            register_code(KC_TAB);
+        } else {
+            unregister_code(KC_TAB);
+        }
+        break;
+    }
+    return true;
+}
+
+void matrix_scan_user(void) {
+    if (is_alt_tab_active) {
+        if (timer_elapsed(alt_tab_timer) > 1000) {
+            unregister_code(KC_LALT);
+            is_alt_tab_active = false;
+        }
+    }
+}
+
+#ifdef OLED_DRIVER_ENABLE
+void oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: " + get_highest_layer(layer_state) + "\n"), false);
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+}
+#endif
